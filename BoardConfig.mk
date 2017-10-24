a40f5324390721995957470ba39b470db8eee90f
Isaac Chen <ycchen@google.com>
2017-10-24 10:09:00 +0300

    j4primelte: Set TARGET_2ND_ARCH_VARIANT to armv8-a
    Setting TARGET_2ND_ARCH_VARIANT to armv8-a, instead of armv7-a-neon,
    can silence the related warning during build.
    
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
