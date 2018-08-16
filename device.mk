27b0990b9c8062000874c6793b9e970f7307d803
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    j4primelte: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -342,6 +342,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
+	
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
 
 # WiFi
 PRODUCT_PACKAGES += \
