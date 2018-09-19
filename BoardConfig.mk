b98a429c8db3f112e892bd011d008e307d3eda1a
Randall Huang <huangrandall@google.com>
2018-09-19 22:35:05 +0800

    j4primelte: Enable partitions on loop devices.
    This is used by the new "virtual disk" feature used by vold to help
    debug and test storage code on devices without physical media.
    
    Bug: 116062201
    Test: builds, boots
    
    Change-Id: I3dc2c31d5efb87e4f7e10791f1d82eb0ef4ee438
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ TARGET_NO_BOOTLOADER := true
 VENDOR_SECURITY_PATCH := 2019-04-01
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive loop.max_part=7
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
