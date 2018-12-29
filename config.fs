6d897ad73012792f478c93790fc56b30460501a0
Brandon Solis <bsolis@codeaurora.org>
2018-12-28 16:06:15 -0800

    j4primelte: Update user for ims daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287
    Signed-off-by: EnesSastim <sastimenes@gmail.com>

--- config.fs
+++ config.fs
@@ -105,8 +105,8 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/imsdatadaemon]
@@ -117,7 +117,7 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
@@ -129,7 +129,7 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
