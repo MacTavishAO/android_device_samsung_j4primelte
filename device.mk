f456e1ae13c6c34fda9a17f493426b13c8c9256c
luk1337 <priv.luk@gmail.com>
2019-08-17 16:28:00 +0000

    j4primelte: Configure FM

--- device.mk
+++ device.mk
@@ -65,6 +65,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/empty:system/etc/empty
 	
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+	
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
