e9565f655cd06354e27650e21da891bf57e19165
TheScarastic <warabhishek@gmail.com>
2019-11-25 07:56:38 +0300

    j4primelte: build libjson
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -272,6 +272,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
     $(LOCAL_PATH)/configs/public.libraries.txt.backup:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt.backup \
+	
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
 
 # QTI
 PRODUCT_COPY_FILES += \
