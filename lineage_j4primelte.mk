edd6a8573f88a7e79f80e3e6d11c224c1a3781e5
Ataberk Özen <ataberkozen123@gmail.com>
2019-11-25 08:19:48 +0300

    j4primelte: update fingerprint and product model
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- lineage_j4primelte.mk
+++ lineage_j4primelte.mk
@@ -1,5 +1,4 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
@@ -12,12 +11,12 @@ PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := j4primelte
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_NAME := lineage_j4primelte
-PRODUCT_MODEL := SM-J415FN
+PRODUCT_MODEL := Galaxy J4+
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := j4primelte
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j4primeltecis-user 9 PPR1.180610.011 J415FNXXU2BSDL release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="4primeltedx-user 9 PPR1.180610.011 J415FXXU2BSDM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := samsung/j4primeltecis/j4primelte:9/PPR1.180610.011/J415FNXXU2BSDL:user/release-keys
+BUILD_FINGERPRINT := "samsung/j4primeltedx/j4primelte:9/PPR1.180610.011/J415FXXU2BSDM:user/release-keys"
