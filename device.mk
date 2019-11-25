b2e23d74a87afda0cea167bd27c95a03c04bd87d
Ataberk Özen <ataberkozen123@gmail.com>
2019-11-25 08:11:17 +0300

    j4primelte: build vibrator hidl
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -346,6 +346,11 @@ PRODUCT_PACKAGES += \
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
+	
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # WiFi
 PRODUCT_PACKAGES += \
