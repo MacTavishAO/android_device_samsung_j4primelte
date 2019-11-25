cb12484d0f8901270a18400902e64857edd85fe7
Ataberk Özen <ataberkozen123@gmail.com>
2019-11-25 07:57:46 +0300

    j4primelte: build renderscript hidl
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -308,6 +308,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     ueventd.qcom.rc \
 	
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+	
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
