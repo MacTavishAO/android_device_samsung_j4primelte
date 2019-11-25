65ceb7bc36657e40a48d03cdbe3e358080646261
Ataberk Özen <ataberkozen123@gmail.com>
2019-11-25 08:05:26 +0300

    j4primelte: build thermal hidls
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -337,6 +337,11 @@ PRODUCT_BOOT_JARS += \
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
+	
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # WiFi
 PRODUCT_PACKAGES += \
