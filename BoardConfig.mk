1ad3145ed89f3201e0672ef4e77a2e2b53d52aa0
Ataberk Özen <ataberkozen123@gmail.com>
2019-11-25 04:25:58 +0300

    j4primelte: include kernel flags
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,19 @@ BOARD_VENDOR := samsung
 # Security patch level
 VENDOR_SECURITY_PATCH := 2019-04-01
 
+# Kernel
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
+TARGET_KERNEL_CONFIG := j4primelte_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
