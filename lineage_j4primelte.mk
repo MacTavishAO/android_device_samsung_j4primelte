4763799c0ba3e5ae64abbc07984107375050427c
Ataberk Özen <ataberkozen123@gmail.com>
2019-11-25 18:52:27 +0300

    j4primelte: fix derp in build description
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- lineage_j4primelte.mk
+++ lineage_j4primelte.mk
@@ -31,7 +31,7 @@ PRODUCT_MODEL := Galaxy J4+
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := j4primelte
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="4primeltedx-user 9 PPR1.180610.011 J415FXXU2BSDM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j4primeltedx-user 9 PPR1.180610.011 J415FXXU2BSDM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "samsung/j4primeltedx/j4primelte:9/PPR1.180610.011/J415FXXU2BSDM:user/release-keys"
