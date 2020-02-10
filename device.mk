0b0f1a10a99b179c9a5fb2bfd78b52432288621e
Erfan Abdi <erfangplus@gmail.com>
2020-02-11 00:45:11 +0330

    j4primelte: Build custom radio service
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -377,7 +377,7 @@ PRODUCT_PACKAGES += \
 	
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
+    android.hardware.radio@1.2-radio-service.samsung \
     android.hardware.radio.config@1.0 \
     libjson \
     librmnetctl \
