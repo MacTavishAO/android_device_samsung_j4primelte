4c481c66a55aeeebcd797ab6ead87d2cf1d18485
Ataberk Özen <ataberkozen123@gmail.com>
2020-02-21 01:58:03 +0300

    j4primelte: nuke custom radio hidl
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -375,7 +375,7 @@ PRODUCT_PACKAGES += \
     
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.3-radio-service.samsung \
+    android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     libjson \
     librmnetctl \
