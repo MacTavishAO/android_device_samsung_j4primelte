3a307162699f8cd6d9cbc9df805c7a28f2b4d283
Ataberk Özen <ataberkozen123@gmail.com>
2020-02-23 06:48:18 +0300

    j4primelte: inherit from msm8917-common
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- ril/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ril.c
-LOCAL_MODULE := libshims_ril
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
